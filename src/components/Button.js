3a7652b9c6791d234fe3dd49eb6ff81761f44f74
Joe Van Gundy <joevangundy@joes-mbp.lan>
2022-06-15 13:42:20 -0500

    fix(button): remove style from button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,5 @@
 import React from "react";
 
 export default function Button({ text, onClick }) {
-  return (
-    <button style={{ color: "red" }} onClick={onClick}>
-      {text}
-    </button>
-  );
+  return <button onClick={onClick}>{text}</button>;
 }

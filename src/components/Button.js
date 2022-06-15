260356262eac161c0049c12c66bfd2f264169896
Joe Van Gundy <joevangundy@joes-mbp.lan>
2022-06-15 13:29:58 -0500

    build(package): adds sematic-release to formailize releases

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Button({ text, onClick }) {
-  return <button onClick={onClick}>{text}</button>;
+  return (
+    <button style={{ color: "red" }} onClick={onClick}>
+      {text}
+    </button>
+  );
 }
